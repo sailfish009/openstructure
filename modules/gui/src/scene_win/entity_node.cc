148be588215b64193d462528fb59de3e77cca1ae
stefan <stefan@5a81b35b-ba03-0410-adc8-b2c5c5119f08>
2010-03-10 08:18:58 +0000

    Added Chains to SceneWin
    git-svn-id: https://dng.biozentrum.unibas.ch/svn/openstructure/trunk@1800 5a81b35b-ba03-0410-adc8-b2c5c5119f08

--- modules/gui/src/scene_win/entity_node.cc
+++ modules/gui/src/scene_win/entity_node.cc
@@ -21,9 +21,12 @@
 #include <QFont>
 #include <QString>
 
+#include <ost/mol/query.hh>
+#include <ost/mol/query_view_wrapper.hh>
+#include <ost/mol/view_type_fw.hh>
+
 #include <ost/gui/gosty_app.hh>
 #include <ost/gui/scene_win/scene_win.hh>
-#include <ost/mol/query_view_wrapper.hh>
 
 #include "current_selection_node.hh"
 #include "entity_part_node.hh"
@@ -42,6 +45,17 @@ EntityNode::EntityNode(gfx::EntityP& entity, SceneNode* parent):
   model->AddNode(parent, this);
 
   new RenderModesNode(entity, this);
+
+  SceneNode* chain_node = new LabelNode("Chains",this);
+  model->AddNode(this, chain_node);
+  mol::EntityView view = entity->GetView();
+  mol::ChainViewList chains = view.GetChainList();
+  for(unsigned int i = 0; i<chains.size();i++){
+    QString name = chains[i].GetName().c_str();
+    name = (name.size() == 0 || name == " ")? "No name" : name;
+    mol::Query query("cname='"+chains[i].GetName()+"'");
+    model->AddNode(chain_node,new EntityPartNode(name,entity, mol::QueryViewWrapper(query,view),chain_node));
+  }
 
   SceneNode* quick_selection = new LabelNode("Quick Selection",this);
   model->AddNode(this, quick_selection);
