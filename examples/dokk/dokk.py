5c9966b0ee8451315e6cd86afdec7b768904994b
tobias <tobias@5a81b35b-ba03-0410-adc8-b2c5c5119f08>
2010-03-12 13:04:58 +0000

    added test files for dokk and modified script
    git-svn-id: https://dng.biozentrum.unibas.ch/svn/openstructure/trunk@1821 5a81b35b-ba03-0410-adc8-b2c5c5119f08

--- examples/dokk/dokk.py
+++ examples/dokk/dokk.py
@@ -1,13 +1,25 @@
 import glwin
 from ost import gfx
 from ost import io
-sdh=io.LoadPDB('../entity/sdh.pdb')
-go=gfx.Entity("sdh", sdh)
-scene.Add(go)
+
+prot=io.LoadPDB('datafiles/2CF8_prot.pdb')
+prot_go=gfx.Entity('prot', prot)
+scene.Add(prot_go)
+
+surf=io.LoadSurface('datafiles/2CF8_surf','msms')
+surf.Attach(prot,5)
+surf_go=gfx.Surface('surf',surf)
+scene.Add(surf_go)
+
+lig=io.LoadSDF('datafiles/2CF8_lig.sdf')
+lig_go=gfx.Entity('lig', lig)
+lig_go.SetRenderMode(gfx.CUSTOM)
+lig_go.SetColor(gfx.GREEN,'ele=C')
+scene.Add(lig_go)
 
 dokk_win=glwin.DokkGLWin()
 
-scene.CenterOn(go)
+scene.CenterOn(lig_go)
 
 if '--fullscreen' in sys.argv:
   dokk_win.Show(fullscreen=True)
