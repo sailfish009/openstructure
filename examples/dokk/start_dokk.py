6c69619e67499734b9e8f694fa9ad973a0fabd14
stefan <stefan@5a81b35b-ba03-0410-adc8-b2c5c5119f08>
2010-04-09 15:05:04 +0000

    dokk (the docking game):  - Renamed some Levels
    git-svn-id: https://dng.biozentrum.unibas.ch/svn/openstructure/trunk@1961 5a81b35b-ba03-0410-adc8-b2c5c5119f08

--- examples/dokk/start_dokk.py
+++ examples/dokk/start_dokk.py
@@ -1,6 +1,6 @@
 from level import Level
 from dokk import Dokk
 dokk=Dokk()
-dokk.SetLevels(['1r6a','1aqb','1bh5','thrombin','thrombinNoH'])
+dokk.SetLevels(['dengue','thrombin','retinol','glyoxalase','thrombinNoH'])
 dokk.Start(sys.argv)
 
