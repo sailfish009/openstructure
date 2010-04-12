abe418e081e62916caeec0cbfc8cdfa518a4258c
stefan <stefan@5a81b35b-ba03-0410-adc8-b2c5c5119f08>
2010-04-12 12:14:19 +0000

    Dokk (the docking game):  - Added tutorial levels
    git-svn-id: https://dng.biozentrum.unibas.ch/svn/openstructure/trunk@1970 5a81b35b-ba03-0410-adc8-b2c5c5119f08

--- examples/dokk/start_dokk.py
+++ examples/dokk/start_dokk.py
@@ -1,6 +1,6 @@
 from level import Level
 from dokk import Dokk
 dokk=Dokk()
-dokk.SetLevels(['dengue','thrombin','retinol','glyoxalase','thrombinNoH'])
+dokk.SetLevels(['tutorial_1','tutorial_2','dengue','thrombin','retinol','glyoxalase','thrombinNoH'])
 dokk.Start(sys.argv)
 
