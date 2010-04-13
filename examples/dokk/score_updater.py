445418e212c2a88451da9bf8dcb386b2737c2e54
tobias <tobias@5a81b35b-ba03-0410-adc8-b2c5c5119f08>
2010-04-13 11:17:20 +0000

    dokk - the game:   -improved speed:      -don't include atoms multiple times      -removed unnecessary hydrogens      -lower surface density   -removed unnecessary files
    git-svn-id: https://dng.biozentrum.unibas.ch/svn/openstructure/trunk@1980 5a81b35b-ba03-0410-adc8-b2c5c5119f08

--- examples/dokk/score_updater.py
+++ examples/dokk/score_updater.py
@@ -6,19 +6,19 @@ class ScoreUpdater(QtCore.QObject):
   def __init__(self, level, parent=None):
     QtCore.QObject.__init__(self, parent)
     self.level = level
-    self.old_atoms = list()
+    self.old_atoms = set()
 
   def UpdateScores(self):
     for atom in self.old_atoms:
       atom.SetGenericFloatProperty('clash', 0)
     
     ligand = self.level.ligand
-    prot_within = list()
+    prot_within = set()
     ligand_atoms = ligand.handle.atoms
     for ligand_atom in ligand_atoms:
       within=self.level.protein.handle.FindWithin(ligand_atom.GetPos(), 3)
       for atom in within:
-        prot_within.append(atom)
+        prot_within.add(atom)
     lig_view=ligand.handle.CreateFullView()
     for a in prot_within:
       score=qa.ClashScore(a, lig_view)
--- examples/dokk/start_dokk.py
+++ examples/dokk/start_dokk.py
@@ -1,6 +1,6 @@
 from level import Level
 from dokk import Dokk
 dokk=Dokk()
-dokk.SetLevels(['tutorial_1','tutorial_2','dengue','thrombin','retinol','glyoxalase','thrombinNoH'])
+dokk.SetLevels(['tutorial_1','tutorial_2','dengue','thrombin','retinol','glyoxalase'])
 dokk.Start(sys.argv)
 
