aac9599f188e22a80f57a968ac28453758c2f30b
stefan <stefan@5a81b35b-ba03-0410-adc8-b2c5c5119f08>
2010-04-13 13:11:11 +0000

    Fixed bug:  Scene Win (bond problem when changing rendermode of backbone and sidechains)
    git-svn-id: https://dng.biozentrum.unibas.ch/svn/openstructure/trunk@1982 5a81b35b-ba03-0410-adc8-b2c5c5119f08

--- modules/gui/src/scene_win/entity_node.cc
+++ modules/gui/src/scene_win/entity_node.cc
@@ -62,7 +62,7 @@ EntityNode::EntityNode(gfx::EntityP& entity, SceneNode* parent):
 
   SceneNode* node = new EntityPartNode("Backbone", entity, mol::QueryViewWrapper(entity->GetView().Select("aname=CA,C,N,O and peptide=true")), quick_selection);
   model->AddNode(quick_selection, node);
-  node = new EntityPartNode("Sidechains", entity, mol::QueryViewWrapper(entity->GetView().Select("aname!=CA,C,N,O and peptide=true")), quick_selection);
+  node = new EntityPartNode("Sidechains", entity, mol::QueryViewWrapper(entity->GetView().Select("aname!=CA,C,N,O and peptide=true",mol::QueryFlag::EXCLUSIVE_BONDS)), quick_selection);
   model->AddNode(quick_selection, node);
   node = new EntityPartNode("Ligands", entity, mol::QueryViewWrapper(entity->GetView().Select("ishetatm=1 and rname!=HOH,WAT")), quick_selection);
   model->AddNode(quick_selection, node);
