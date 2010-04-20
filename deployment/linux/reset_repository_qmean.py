600a3a9ceaff74865cd9a58d4bf76b6ecb1da532
valerio <valerio@5a81b35b-ba03-0410-adc8-b2c5c5119f08>
2010-04-20 16:57:23 +0000

    Scripts to create qmean bubndle
    git-svn-id: https://dng.biozentrum.unibas.ch/svn/openstructure/trunk@2066 5a81b35b-ba03-0410-adc8-b2c5c5119f08

--- deployment/linux/reset_repository_qmean.py
+++ deployment/linux/reset_repository_qmean.py
@@ -8,7 +8,7 @@ if currdir.find('deployment')==-1 or currdir.find('linux')==-1:
   print '\n'
   sys.exit()
 
-subprocess.call('rm -fr scripts/dng.in.pre* scripts/ost_cl.in.pre*',shell=True,cwd='../../')
+subprocess.call('rm -fr scripts/dng.in.pre* scripts/ost_cl.in.pre* modules/scratch/qmean/scripts/qmean.in.pre*',shell=True,cwd='../../')
 subprocess.call('mv scripts/ost.in.backup scripts/ost.in',shell=True,cwd='../../')
 subprocess.call('mv scripts/dng.in.backup scripts/dng.in',shell=True,cwd='../../')
-
+subprocess.call('mv modules/scratch/qmean/scripts/qmean.in.backup scripts/dng.in modules/scratch/qmean/scripts/qmean.in',shell=True,cwd='../../')
