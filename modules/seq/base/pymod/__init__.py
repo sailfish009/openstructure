ae555fd4563dc97ff95b00a0c6294ebe850cbf54
juergen <juergen@5a81b35b-ba03-0410-adc8-b2c5c5119f08>
2010-05-12 18:51:48 +0000

    updating unit tests for viewsfromseq
    git-svn-id: https://dng.biozentrum.unibas.ch/svn/openstructure/trunk@2253 5a81b35b-ba03-0410-adc8-b2c5c5119f08

--- modules/seq/base/pymod/__init__.py
+++ modules/seq/base/pymod/__init__.py
@@ -19,16 +19,20 @@
 from _seq import *
 from ost import mol
 
+## \brief Create two views from sequence alignment such that the matching 
+#      residues occur at the same index within the chain.
+#
+# The first sequence in the sequence alignment belongs to ent_a, the second to
+# ent_b.
+#
+# Requires that each of the entities/views only contains one chain and that
+# the sequence offset of the sequences in the alignment is set appropriately.
 def ViewsFromAlignment(multi_seq_ali, ent_a, ent_b, include_atoms=True):
   seq_a=multi_seq_ali.GetSequence(0)
   seq_b=multi_seq_ali.GetSequence(1)
-
-  # set offset according to ent_a and ent_b
-
-
   return ViewsFromSequences(seq_a, seq_b, ent_a, ent_b, include_atoms)
 
-def ViewsFromSequences(seq_a, seq_b, ent_a=None, ent_b=None,
+def ViewsFromSequences(seq_a, seq_b, ent_a=None, ent_b=None, 
                        include_atoms=True):
   ent_a=ent_a or seq_a.attached_view
   ent_b=ent_b or seq_b.attached_view
@@ -37,81 +41,42 @@ def ViewsFromSequences(seq_a, seq_b, ent_a=None, ent_b=None,
   if ent_b.chain_count>1:
    raise RuntimeError("second entity contains more than one chain")
   is_ha=isinstance(ent_a, mol.EntityHandle)
-  is_hb=isinstance(ent_b, mol.EntityHandle)
+  is_hb=isinstance(ent_b, mol.EntityHandle)  
 
   ev_a= is_ha and ent_a.CreateEmptyView() or ent_a.handle.CreateEmptyView()
   ev_b= is_hb and ent_b.CreateEmptyView() or ent_b.handle.CreateEmptyView()
 
   flags=include_atoms and mol.ViewAddFlag.INCLUDE_ATOMS or 0
-  # get all residues
   res_a=ent_a.residues
   res_b=ent_b.residues
-
-  # map sequence onto
-  s1 = seq_a.gapless_string
-  s2 = seq_b.gapless_string
-
-  # get sequence of ent1
-  seq_ent_a = ""
-  seq_ent_b= ""
-
-  sub_a = ent_a.Select("peptide = 1")
-  sub_b = ent_b.Select("peptide = 1")
-  for res1 in sub_a.residues:
-      seq_ent_a = "%s%s" % (seq_ent_a, res1.one_letter_code)
-
-  seq_ent_b = ""
-  for res2 in sub_b.residues:
-      seq_ent_b = "%s%s" % (seq_ent_b, res2.one_letter_code)
-
-  #~ print seq_ent_a
-  #~ print s1
-
-  #~ print seq_ent_b
-  #~ print s2
-  off_a = seq_ent_a.index(s1)
-  off_b = seq_ent_b.index(s2)
-
-  #off_a=seq_a.GetSequenceOffset()
-  #off_b=seq_b.GetSequenceOffset()
-
+  off_a=seq_a.GetSequenceOffset()
+  off_b=seq_b.GetSequenceOffset()
 
   index_a, index_b=(0, 0)
   done=False
-
-  align_index = 0
-
-  # iterate over residues
+  #~ for i,j in zip(ent_a.residues,ent_b.residues): print i,i.index,j,j.index
+  #~ print  
   while index_a<seq_a.GetLength() and index_b<seq_b.GetLength():
-      # print 'index',index_a, seq_a.GetLength(), index_b, seq_b.GetLength()
-    while seq_a.GetOneLetterCode(index_a)=='-':
-     index_a+=1
-     if seq_a.GetLength()<index_a:
-       done=True
-       break
-    while seq_b.GetOneLetterCode(index_b)=='-':
-     index_b+=1
-     if seq_b.GetLength()<index_b:
-       done=True
-       break
-    if done or len(res_a)<=off_a+index_b or len(res_b)<=off_b+index_a:
-     break
+    while seq_a.GetOneLetterCode(index_a+off_b)=='-':
+      index_a+=1
+      print 'gap a'
+      if seq_a.GetLength()<=index_a+off_b:
+        done=True        
+        break
+    while seq_b.GetOneLetterCode(index_b+off_a)=='-':
+      index_b+=1
+      print 'gap b'
+      if seq_b.GetLength()<=index_b+off_a:
+        done=True
+        break
+    if done or len(res_a)<=index_b+off_a or len(res_b)<=index_a+off_b:
+      break
     ra=res_a[off_a+index_b]
     rb=res_b[off_b+index_a]
-    #ra = res_a[index_b]
-    #rb = res_a[index_a]
-    #~ print "set align_index %d for %s %s" % (align_index, ra, rb)
-    #ra.SetIntProp("align_index",align_index)
-    #rb.SetIntProp("align_index",align_index)
-    align_index += 1
+    print 'index_a',index_a, seq_a.GetLength(),seq_a.GetOneLetterCode(index_a+off_a),ra
+    print 'index_b',index_b, seq_b.GetLength(),seq_b.GetOneLetterCode(index_b+off_b),rb
     ev_a.AddResidue(is_ha and ra or ra.handle, flags)
     ev_b.AddResidue(is_hb and rb or rb.handle, flags)
     index_a+=1
     index_b+=1
-  if (ent_a.chains[0].name!=ev_a.chains[0].name):
-    eda=ev_a.handle.RequestXCSEditor()
-    eda.RenameChain(ent_a.chains[0].handle,ent_a.chains[0].name)
-  if  (ent_b.chains[0].name!=ev_b.chains[0].name): 
-    edb=ev_b.handle.RequestXCSEditor()
-    edb.RenameChain(ent_b.chains[0].handle, ent_b.chains[0].name)
   return ev_a, ev_b
--- modules/seq/base/tests/test_seq.py
+++ modules/seq/base/tests/test_seq.py
@@ -3,133 +3,175 @@ from ost import *
 from ost import settings
 from ost import seq
 
+
+def fixture(seq_str='MRLDGKTALITGSAR'):
+  e=mol.CreateEntity()
+  ede=e.RequestXCSEditor()
+  chain=ede.InsertChain('A')
+  res_str='MET ARG LEU ASP GLY LYS THR ALA LEU ILE THR GLY SER ALA ARG'
+  res=res_str.split()
+  olc={}
+  counter=0
+  for i in 'MRLDGKTALITGSAR':
+    olc[i]=res[counter]
+    counter+=1
+  
+  for i in seq_str:
+    if i!='-':
+      r=ede.AppendResidue(chain,olc[i])
+      r.SetOneLetterCode(i)
+      a=ede.InsertAtom(r,'CA',geom.Vec3())
+  #~ for i in e.atoms: print i,i.index
+  return e.Copy()
+  
 class TestSeq(unittest.TestCase):
   
   def setUp(self):
-    self.protein = io.LoadEntity("testfiles/testprotein.pdb")
+    #sequence and entity must correspond
+    print 'initialising'
+    ent=fixture()
+    self.protein=ent.Select('')
     c=self.protein.chains[0]
     self.seq_a=seq.SequenceFromChain('0',c) # MRLDGKTALITGSAR
-    self.seq_a.AttachView(self.protein.Select('aname==CA'))
+    self.seq_a.AttachView(self.protein)
     self.seq_b=seq.CreateSequence('1','---DGKTALITGSAR')
-    self.seq_b.AttachView(self.protein.Select('aname==CA'))
-    self.seq_c=seq.CreateSequence('2','MRLDG---LITGSAR')
-    self.seq_c.AttachView(self.protein.Select('aname==CA'))
-    self.seq_d=seq.CreateSequence('3','MRLDG----ITGSAR')
-    self.seq_d.AttachView(self.protein.Select('aname==CA'))
+    self.seq_b.AttachView(fixture('---DGKTALITGSAR').Select(''))
+    #~ self.seq_c=seq.CreateSequence('2','MRLDG---LITGSAR')
+    #~ self.seq_c.AttachView(self.protein)
+    #~ self.seq_d=seq.CreateSequence('3','MRLDG----ITGSAR')
+    #~ self.seq_d.AttachView(self.protein)
     self.seq_e=seq.CreateSequence('4','AMRLDG----ITGSA')
-    self.seq_e.AttachView(self.protein.Select('aname==CA'))
+    self.seq_e.AttachView(fixture('AMRLDG----ITGSA').Select(''))
+    ##self.seq_e.AttachView(self.protein)
     self.seq_e.SetSequenceOffset(1)
     self.seq_f=seq.CreateSequence('5','MRLDGKTA-ITGSAR')
-    self.seq_f.AttachView(self.protein.Select('aname==CA'))
+    self.seq_f.AttachView(fixture('MRLDGKTA-ITGSAR').Select(''))
     self.seq_g=seq.CreateSequence('6','MRLDGKTALITG---')
-    self.seq_g.AttachView(self.protein.Select('aname==CA'))
+    self.seq_g.AttachView(fixture('MRLDGKTALITG---').Select(''))
     
-  def testViewsFromSequencesGapFrontSeqB(self):
-    #~ MRLDGKTALITGSAR
-    #~ ---DGKTALITGSAR
-    [a,b]=seq.ViewsFromSequences(self.seq_a,
-                                 self.seq_b,
-                                 self.seq_a.GetAttachedView(),
-                                 self.seq_b.GetAttachedView())
-    self.assertEqual(a.atoms[0].GetResidue().GetOneLetterCode(),'D');
-    self.assertEqual(b.atoms[0].GetResidue().GetOneLetterCode(),'D');
-
-  def testViewsFromSequencesGapFrontSeqA(self):
-    #~ ---DGKTALITGSAR
-    #~ MRLDGKTALITGSAR
-    [a,b]=seq.ViewsFromSequences(self.seq_b,
-                                 self.seq_a,
-                                 self.seq_b.GetAttachedView(),
-                                 self.seq_a.GetAttachedView())
-    for i,j in zip(a.atoms,b.atoms): print i,j
-    self.assertEqual(a.atoms[0].GetResidue().GetOneLetterCode(),'D');
-    self.assertEqual(b.atoms[0].GetResidue().GetOneLetterCode(),'D');
-
-
-  def testViewsFromSequencesGapRearSeqB(self):
-    #~ MRLDGKTALITGSAR
-    #~ ---DGKTALITGSAR
+#~ MRLDGKTALITGSAR
+#~ ---DGKTALITGSAR
+  def testViewsFromSequences1(self):
+    
     [a,b]=seq.ViewsFromSequences(self.seq_a,
                                  self.seq_b,
                                  self.seq_a.GetAttachedView(),
                                  self.seq_b.GetAttachedView())
 
+    #~ for i,j in zip(a.atoms,b.atoms):
+      #~ print i,i.residue.one_letter_code,j,j.residue.one_letter_code
+    
     self.assertEqual(a.atoms[0].GetResidue().GetOneLetterCode(),'D');
     self.assertEqual(b.atoms[0].GetResidue().GetOneLetterCode(),'D');
+    self.assertEqual(a.atoms[11].GetResidue().GetOneLetterCode(),'R');
+    self.assertEqual(b.atoms[11].GetResidue().GetOneLetterCode(),'R');
 
-  def testViewsFromSequencesGapRearSeqA(self):
-    #~ ---DGKTALITGSAR
-    #~ MRLDGKTALITGSAR
+#~ ---DGKTALITGSAR
+#~ MRLDGKTALITGSAR
+  def testViewsFromSequences2(self):
     [a,b]=seq.ViewsFromSequences(self.seq_b,
                                  self.seq_a,
                                  self.seq_b.GetAttachedView(),
                                  self.seq_a.GetAttachedView())
-    #~ for i,j in zip(a.atoms,b.atoms): print i,j
+
     self.assertEqual(a.atoms[0].GetResidue().GetOneLetterCode(),'D');
     self.assertEqual(b.atoms[0].GetResidue().GetOneLetterCode(),'D');
+    self.assertEqual(a.atoms[11].GetResidue().GetOneLetterCode(),'R');
+    self.assertEqual(b.atoms[11].GetResidue().GetOneLetterCode(),'R');
+
+
+##~ MRLDGKTALITGSAR
+##~ MRLDGKTALITG---
+  def testViewsFromSequences3(self):
+    [a,b]=seq.ViewsFromSequences(self.seq_a,
+                                 self.seq_g,
+                                 self.seq_a.GetAttachedView(),
+                                 self.seq_g.GetAttachedView())
+
+    self.assertEqual(a.atoms[0].GetResidue().GetOneLetterCode(),'M');
+    self.assertEqual(b.atoms[0].GetResidue().GetOneLetterCode(),'M');
+    self.assertEqual(a.atoms[-1].GetResidue().GetOneLetterCode(),'G');
+    self.assertEqual(b.atoms[-1].GetResidue().GetOneLetterCode(),'G');
+    
+
+##~ MRLDGKTALITG---
+##~ MRLDGKTALITGSAR
+  def testViewsFromSequences4(self):
+    [a,b]=seq.ViewsFromSequences(self.seq_g,
+                                 self.seq_a,
+                                 self.seq_g.GetAttachedView(),
+                                 self.seq_a.GetAttachedView())
+
+    self.assertEqual(a.atoms[0].GetResidue().GetOneLetterCode(),'M');
+    self.assertEqual(b.atoms[0].GetResidue().GetOneLetterCode(),'M');
+    self.assertEqual(a.atoms[-1].GetResidue().GetOneLetterCode(),'G');
+    self.assertEqual(b.atoms[-1].GetResidue().GetOneLetterCode(),'G');
 
 
 
-  def testViewsFromSequencesSingleGapMiddle1(self):
-    #~ MRLDGKTALITGSAR
-    #~ MRLDGKTA-ITGSAR
+##~ MRLDGKTALITGSAR
+##~ MRLDGKTA-ITGSAR
+  def testViewsFromSequences5(self):
     [a,b]=seq.ViewsFromSequences(self.seq_a,
                                  self.seq_f,
                                  self.seq_a.GetAttachedView(),
                                  self.seq_f.GetAttachedView())
+    print 'result'
+    for i,j in zip(a.residues,b.residues): print i,i.index,j,j.index
+                                 
     self.assertEqual(a.atoms[0].GetResidue().GetOneLetterCode(),'M');
     self.assertEqual(b.atoms[0].GetResidue().GetOneLetterCode(),'M');
+    #~ for i,j in zip(a.atoms,b.atoms): print i,j
     self.assertEqual(a.atoms[8].GetResidue().GetOneLetterCode(),'I');
     self.assertEqual(b.atoms[8].GetResidue().GetOneLetterCode(),'I');
 
-  def testViewsFromSequencesSingleGapMiddle2(self):
-    #~ MRLDGKTA-ITGSAR
-    #~ MRLDGKTALITGSAR
-    [a,b]=seq.ViewsFromSequences(self.seq_f,
-                                 self.seq_a,
-                                 self.seq_f.GetAttachedView(),
-                                 self.seq_a.GetAttachedView())
+##~ MRLDGKTA-ITGSAR
+##~ MRLDGKTALITGSAR
+  #def testViewsFromSequences6(self):
+    #[a,b]=seq.ViewsFromSequences(self.seq_f,
+                                 #self.seq_a,
+                                 #self.seq_f.GetAttachedView(),
+                                 #self.seq_a.GetAttachedView())
 
-    self.assertEqual(a.atoms[0].GetResidue().GetOneLetterCode(),'M');
-    self.assertEqual(b.atoms[0].GetResidue().GetOneLetterCode(),'M');
-    self.assertEqual(a.atoms[8].GetResidue().GetOneLetterCode(),'I');
-    self.assertEqual(b.atoms[8].GetResidue().GetOneLetterCode(),'I');
-  #def testViewsFromSequencesGapMiddleBoth(self):
-    ##~ MRLDG---LITGSAR
-    ##~ MRLDG----ITGSAR
-    #[a,b]=seq.ViewsFromSequences(self.seq_c,
-                                 #self.seq_d,
-                                 #self.seq_c.GetAttachedView(),
-                                 #self.seq_d.GetAttachedView())
-
-    #print 'aaa'+a.atoms[4].name+'aaaa'
-    #assertEqual(a.atoms[0].GetResidue().GetOneLetterCode()=='M');
-    #assertEqual(b.atoms[0].GetResidue().GetOneLetterCode()=='M');
-    #assertEqual(a.atoms[4].GetResidue().GetOneLetterCode()=='I');
-    #assertEqual(b.atoms[4].GetResidue().GetOneLetterCode()=='I');
-                  #~ self.seq_b.GetResidue(j.GetResidue().GetIndex()).GetOneLetterCode());
+    #self.assertEqual(a.atoms[0].GetResidue().GetOneLetterCode(),'M');
+    #self.assertEqual(b.atoms[0].GetResidue().GetOneLetterCode(),'M');
+    #self.assertEqual(a.atoms[8].GetResidue().GetOneLetterCode(),'I');
+    #self.assertEqual(b.atoms[8].GetResidue().GetOneLetterCode(),'I');
+    
+##~ MRLDG---LITGSAR
+##~ MRLDG----ITGSAR
+  #~ def testViewsFromSequences7(self):
+    #~ [a,b]=seq.ViewsFromSequences(self.seq_c,
+                                 #~ self.seq_d,
+                                 #~ self.seq_c.GetAttachedView(),
+                                 #~ self.seq_d.GetAttachedView())
+#~ 
+    #~ self.assertEqual(a.atoms[0].GetResidue().GetOneLetterCode(),'M');
+    #~ self.assertEqual(b.atoms[0].GetResidue().GetOneLetterCode(),'M');
+    #~ self.assertEqual(a.atoms[5].GetResidue().GetOneLetterCode(),'I');
+    #~ self.assertEqual(b.atoms[5].GetResidue().GetOneLetterCode(),'I');
                   
-  #def testViewsFromSequencesGapSeqOffset(self):
-    ##~ MRLDGKTALITGSAR
-    ##~ AMRLDG----ITGSA
-    #[a,b]=seq.ViewsFromSequences(self.seq_a,
-                                 #self.seq_e,
-                                 #self.seq_a.GetAttachedView(),
-                                 #self.seq_e.GetAttachedView())
-
-
-    #assertEqual(a.atoms[0].GetResidue().GetOneLetterCode()=='M');
-    #assertEqual(b.atoms[0].GetResidue().GetOneLetterCode()=='M');
-    #assertEqual(a.atoms[5].GetResidue().GetOneLetterCode()=='I');
-    #assertEqual(b.atoms[5].GetResidue().GetOneLetterCode()=='I');
-                  ##~ self.seq_b.GetResidue(j.GetResidue().GetIndex()).GetOneLetterCode());
+#~ MRLDGKTALITGSAR
+#~ AMRLDG----ITGSA
+  def testViewsFromSequences8(self):
+    [a,b]=seq.ViewsFromSequences(self.seq_a,
+                                 self.seq_e,
+                                 self.seq_a.GetAttachedView(),
+                                 self.seq_e.GetAttachedView())
+    print 'result'
+    for i,j in zip(a.residues,b.residues): print i,i.index,j,j.index
+    self.assertEqual(a.atoms[0].GetResidue().GetOneLetterCode(),'M');
+    self.assertEqual(b.atoms[0].GetResidue().GetOneLetterCode(),'M');
+    self.assertEqual(a.atoms[6].GetResidue().GetOneLetterCode(),'I');
+    self.assertEqual(b.atoms[6].GetResidue().GetOneLetterCode(),'I');
+               
  
 
 
 if __name__ == "__main__":
   # test if python extension for seq module work
   suite = unittest.TestLoader().loadTestsFromTestCase(TestSeq)
-  unittest.TextTestRunner(verbosity=2).run(suite)
+  unittest.TextTestRunner(verbosity=3).run(suite)
 
   #~ unittest.main()
 
