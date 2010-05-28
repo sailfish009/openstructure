0bf94b6aaaf05c2f5cff829a3ab5cb091fb70f3c
stefan <stefan@5a81b35b-ba03-0410-adc8-b2c5c5119f08>
2010-05-28 08:04:16 +0000

    CTest, changed Nightly build time from 00:00:00 EST to 00:00:00 MEZ
    git-svn-id: https://dng.biozentrum.unibas.ch/svn/openstructure/trunk@2294 5a81b35b-ba03-0410-adc8-b2c5c5119f08

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -5,7 +5,7 @@
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
 set(CTEST_PROJECT_NAME "OpenStructure")
-set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 MEZ")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "kb-pdwg1059-06.pz.unibas.ch")
