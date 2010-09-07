dfc52ee69a26403e875f1dc24d65ca5c956bd95f
valerio <valerio@5a81b35b-ba03-0410-adc8-b2c5c5119f08>
2010-09-07 23:25:41 +0000

    Inverted two include statements
    The newest gcc on Mac likes it better this way! Don't ask me!
    
    git-svn-id: https://dng.biozentrum.unibas.ch/svn/openstructure/trunk@2695 5a81b35b-ba03-0410-adc8-b2c5c5119f08

--- modules/base/pymod/wrap_base.cc
+++ modules/base/pymod/wrap_base.cc
@@ -16,10 +16,9 @@
 // along with this library; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 //------------------------------------------------------------------------------
-#include <vector>
-
 #include <boost/python.hpp>
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+#include <vector>
 #include <ost/export_helper/vector.hh>
 #include <ost/base.hh>
 #include <ost/platform.hh>
