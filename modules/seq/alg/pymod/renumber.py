e0634f2d247b0fd22cb900f0476ecaa9e52b4fcb
pascal <pascal@5a81b35b-ba03-0410-adc8-b2c5c5119f08>
2010-09-08 07:14:49 +0000

    renumbering an entity according to an alignment. Input sequence handle with attached view
    git-svn-id: https://dng.biozentrum.unibas.ch/svn/openstructure/trunk@2696 5a81b35b-ba03-0410-adc8-b2c5c5119f08

---
+++ modules/seq/alg/pymod/renumber.py
@@ -0,0 +1,51 @@
+from ost import io, seq, mol, conop
+
+def Renumber(seq_handle):
+  """
+  Function to renumber an entity according to an alignment between the model sequence 
+  and the full-length target sequence. The aligned model sequence with an attached 
+  view needs to be provided. Upon succcess, the renumbered entity is returned.
+
+  .. code-block:: python
+
+    from ost.seq.alg import renumber
+    from ost.bindings.clustalw import *
+    ent=io.LoadPDB("path_to_model")
+    s=io.LoadSequence("path_to_full_length_fasta_seqeunce")
+    pdb_seq=seq.SequenceFromChain("model", ent.chains[0])
+    aln=ClustalW(s,pdb_seq)
+    aln.AttachView(1,ent.chains[0].Select(""))
+    e=Renumber(aln.GetSequence(1))
+    io.SavePDB(e, "renum.pdb")
+   
+  """
+  if seq_handle.HasAttachedView==False:
+    raise RuntimeError, "Sequence Handle has no attached view"
+  counter=0
+  changed_residue_count=0
+  renumberingFlag = 0
+  ent_n=mol.CreateEntity()
+  ed=ent_n.RequestXCSEditor()
+  c=ed.InsertChain(" ")
+  for pos in range(len(seq_handle)):
+    if seq_handle[pos]!='-':
+      r=seq_handle.attached_view.residues[counter]
+      if r.IsValid():
+          #print seq_handle[pos],r.number.num,pos+1
+          if r.number.num!=pos+1:
+            changed_residue_count+=1
+            renumberingFlag = 1
+          r_n=ed.AppendResidue(c,r.name, mol.ResNum(pos+1))
+          for atom in r.atoms:
+            ed.InsertAtom(r_n,atom.name,atom.pos,atom.prop)
+      else:
+        err='Error: renumbering failed at position %s' %pos
+        raise RuntimeError, err
+      counter+=1
+  if renumberingFlag == 1:
+    err = 'Warning: %s residues have been renumbered!' %changed_residue_count
+    print err
+  conop.ConnectAll(ent_n)
+  return ent_n
+
+
