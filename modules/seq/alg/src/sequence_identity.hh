415ccd23d5aa3132550966ce33ac3e9f05f77b4e
Marco Biasini <marco.biasini@unibas.ch>
2010-10-05 09:40:18 +0200

    fix include guard

--- modules/seq/alg/src/sequence_identity.hh
+++ modules/seq/alg/src/sequence_identity.hh
@@ -16,7 +16,7 @@
 // along with this library; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 //------------------------------------------------------------------------------
-#ifndef OST_SEQ_SEQUQENCE_IDENTIY_HH
+#ifndef OST_SEQ_SEQUENCE_IDENTITY_HH
 #define OST_SEQ_SEQUENCE_IDENTITY_HH
 
 /*  
