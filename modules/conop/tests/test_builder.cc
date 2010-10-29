1694fab01bf42e99094e2257794a678405d47b8b
Marco Biasini <marco.biasini@unibas.ch>
2010-10-29 16:34:51 +0200

    skip leading numeric characters when guessing element

--- modules/conop/tests/test_builder.cc
+++ modules/conop/tests/test_builder.cc
@@ -34,14 +34,23 @@ BOOST_AUTO_TEST_CASE( test_builder )
 {
   BOOST_CHECK_EQUAL(Builder::GuessAtomElement("CA", false), "C");
   BOOST_CHECK_EQUAL(Builder::GuessAtomElement("CB", false), "C");
-  BOOST_CHECK_EQUAL(Builder::GuessAtomElement("N", false), "N");  
+  BOOST_CHECK_EQUAL(Builder::GuessAtomElement("N", false), "N");
   BOOST_CHECK_EQUAL(Builder::GuessAtomElement("O", false), "O");
   BOOST_CHECK_EQUAL(Builder::GuessAtomElement("CG1", false), "C");
   BOOST_CHECK_EQUAL(Builder::GuessAtomElement("CG2", false), "C");
-  BOOST_CHECK_EQUAL(Builder::GuessAtomElement("OG1", false), "O");  
+  BOOST_CHECK_EQUAL(Builder::GuessAtomElement("OG1", false), "O");
   BOOST_CHECK_EQUAL(Builder::GuessAtomElement("SG", false), "S");
-  
+  BOOST_CHECK_EQUAL(Builder::GuessAtomElement("1HA", false), "H");
+  BOOST_CHECK_EQUAL(Builder::GuessAtomElement("1HB", false), "H");
+  BOOST_CHECK_EQUAL(Builder::GuessAtomElement("1DA", false), "D");
+  BOOST_CHECK_EQUAL(Builder::GuessAtomElement("1DB", false), "D");
+  BOOST_CHECK_EQUAL(Builder::GuessAtomElement("1HA", true), "H");
+  BOOST_CHECK_EQUAL(Builder::GuessAtomElement("1HB", true), "H");
+  BOOST_CHECK_EQUAL(Builder::GuessAtomElement("1DA", true), "D");
+  BOOST_CHECK_EQUAL(Builder::GuessAtomElement("1DB", true), "D");  
   BOOST_CHECK_EQUAL(Builder::GuessAtomElement("CA", true), "CA");
+  BOOST_CHECK_EQUAL(Builder::GuessAtomElement("11", true), "");
+  BOOST_CHECK_EQUAL(Builder::GuessAtomElement("11", false), "");  
 }
 
 BOOST_AUTO_TEST_SUITE_END()
