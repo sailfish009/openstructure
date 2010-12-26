f917fa477b6fc139387f4c05ed7012d56470c690
Marco Biasini <marco.biasini@unibas.ch>
2010-12-26 12:46:03 +0100

    Don't display profile normals in SLINE mode

--- modules/gfx/src/impl/sline_renderer.cc
+++ modules/gfx/src/impl/sline_renderer.cc
@@ -83,7 +83,7 @@ void SlineRenderer::PrepareRendering(const BackboneTrace& trace_subset,
         VertexID p1 = va.Add(sit->position, geom::Vec3(),sit->color1);
         va.AddLine(p0,p1);
         p0=p1;
-#if 1
+#if 0
         VertexID p2 = va.Add(sit->position+sit->direction,geom::Vec3(),Color(0,1,0));
         VertexID p3 = va.Add(sit->position+sit->normal,geom::Vec3(),Color(1,0,0));
         VertexID p4 = va.Add(sit->position+sit->v2,geom::Vec3(),Color(1,0,1));
