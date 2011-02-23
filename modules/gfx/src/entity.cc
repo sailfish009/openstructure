sailfish009/openstructure

commit b9fd3da98d0d3a261df02598e2c2407c7bc8d73c

fixed default outline (again); fixed superfluous rebuild for hidden objects

modules/gfx/src/entity.cc
@@ -75,7 +75,8 @@ Entity::Entity(const String& name,
   opacity_(1.0),
   blur_(false),
   blurf1_(1.0),
-  blurf2_(0.8)
+  blurf2_(0.8),
+  needs_update_(true)
 {
   init(RenderMode::SIMPLE);
 }
@@ -327,26 +328,29 @@ void Entity::CacheBoundingBox() const
 bool Entity::UpdateIfNeeded() const
 {
   bool updated=false;
-  for (RendererMap::iterator i=renderer_.begin(), 
-       e=renderer_.end(); i!=e; ++i) {
-    EntityRenderer* renderer =i->second;
-    if (renderer->IsDirty()) {
-      renderer->PrepareRendering();
-      updated=true;
+  if(IsVisible()) {
+    for (RendererMap::iterator i=renderer_.begin(), 
+           e=renderer_.end(); i!=e; ++i) {
+      EntityRenderer* renderer =i->second;
+      if (renderer->IsDirty()) {
+        renderer->PrepareRendering();
+        updated=true;
+      }
+      renderer->VA().SetOpacity(opacity_);
     }
-    renderer->VA().SetOpacity(opacity_);
-  }
-  if (updated) {
-    this->CacheBoundingBox();
+    if (updated) {
+      this->CacheBoundingBox();
+    }
   }
   return updated;
 }
 
 void Entity::CustomPreRenderGL(bool update)
 {
-  if (update) {
+  if (update || needs_update_) {
     this->UpdateIfNeeded();
     RefreshVA();
+    needs_update_=false;
   }
 }
 

modules/gfx/src/entity.hh
@@ -311,6 +311,7 @@ private:
   bool blur_;
   float blurf1_;
   float blurf2_;
+  bool needs_update_;
 };
 
 

modules/gfx/pymod/__init__.py
@@ -183,4 +183,4 @@ def _Match(scene, pattern="*"):
     return matches
   return GfxNodeListProxy(_Recurse("", Scene().root_node, pattern))
 
-SceneSingleton.__getitem__=_Match
+SceneSingleton.Match=_Match

modules/gfx/src/gfx_object.cc
@@ -58,7 +58,11 @@ GfxObj::GfxObj(const String& name):
   opacity_(1.0),
   smoothf_(0.0),
   outline_flag_(false),
+#if defined (__APPLE__)
+  outline_mode_(2),
+#else
   outline_mode_(1),
+#endif
   c_ops_(),
   labels_(),
   use_occlusion_(false)

modules/gfx/src/vertex_array.cc
@@ -437,7 +437,7 @@ void IndexedVertexArray::RenderGL()
     glCullFace(GL_FRONT);
     glEnable(GL_CULL_FACE);
     glShadeModel(GL_FLAT);
-    if(outline_mode_==2) {
+    if(outline_mode_==1) {
       glCallList(outline_mat_dlist_);
       glEnable(GL_POLYGON_OFFSET_LINE);
       glEnable(GL_POLYGON_OFFSET_POINT);
@@ -447,7 +447,7 @@ void IndexedVertexArray::RenderGL()
       glEnable(GL_LINE_SMOOTH);
       glDisable(GL_POINT_SMOOTH); // kills selfx fragment shader if enabled
       glDisable(GL_POLYGON_SMOOTH);
-    } else if(outline_mode_==1) {
+    } else if(outline_mode_==2) {
       glCallList(outline_mat_dlist_);
       glEnable(GL_POLYGON_OFFSET_LINE);
       glPolygonOffset(10.0,1.0);

commit fd461a860aed424d6fcac523f5595579a3e46029

fixed bug introduced in b9fd3da98d (fixed superfluous rebuild of hidden objects)

modules/gfx/src/entity.cc
@@ -341,6 +341,9 @@ bool Entity::UpdateIfNeeded() const
     if (updated) {
       this->CacheBoundingBox();
     }
+    needs_update_=false;
+  } else {
+    needs_update_=true;
   }
   return updated;
 }

modules/gfx/src/entity.hh
@@ -311,7 +311,7 @@ private:
   bool blur_;
   float blurf1_;
   float blurf2_;
-  bool needs_update_;
+  mutable bool needs_update_;
 };
 
 