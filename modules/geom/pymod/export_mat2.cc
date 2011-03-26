a30d20be0491ee6b3d87d380daecc71af4975d18
Marco Biasini <marco.biasini@unibas.ch>
2011-03-26 22:35:41 +0100

    added repr() for vector and matrix types

--- modules/geom/pymod/export_mat2.cc
+++ modules/geom/pymod/export_mat2.cc
@@ -26,7 +26,13 @@ using namespace boost::python;
 const Real Mat2_getitem(const geom::Mat2& m, tuple i) {return m(extract<int> (i[0]),extract<int> (i[1]));}
 void Mat2_setitem(geom::Mat2& m,const  tuple i,const  Real val) {m(extract<int> (i[0]),extract<int> (i[1]))=val;}
 
+String mat2_repr(const geom::Mat2& m) {
+  std::stringstream ss;
 
+  ss << "geom.Mat2(" << m(0,0) << ", " << m(0,1) << ", "
+     << m(1,0) << ", " << m(1,1) << ")";
+  return ss.str();
+}
 void export_Mat2()
 {
   using namespace geom;
@@ -36,6 +42,7 @@ void export_Mat2()
     .def(self += self)
     .def(self -= self)
     .def(self + self)
+    .def("__repr__", mat2_repr)
     .def(self - self)
     .def(self *= Real())
     .def(self /= Real())
---
+++ modules/geom/tests/test_repr.py
@@ -0,0 +1,39 @@
+import unittest
+from ost import geom
+
+class TestRepr(unittest.TestCase):
+  def testReprVec2(self):
+    v=geom.Vec2(1,2)
+    v2=eval(repr(v))
+    assert geom.Equal(v, v2)
+
+  def testReprVec3(self):
+    v=geom.Vec3(1,2,3)
+    v2=eval(repr(v))
+    assert geom.Equal(v, v2)
+
+  def testReprVec4(self):
+    v=geom.Vec4(1,2,3,4)
+    v2=eval(repr(v))
+    assert geom.Equal(v, v2)
+
+  def testReprMat2(self):
+    m=geom.Mat2(1,2,3,4)
+    m2=eval(repr(m))
+    assert geom.Equal(m, m2)
+
+  def testReprMat3(self):
+    m=geom.Mat3(1,2,3,4,5,6,7,8,9)
+    m2=eval(repr(m))
+    assert geom.Equal(m, m2)
+
+  def testReprMat4(self):
+    m=geom.Mat4(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+    m2=eval(repr(m))
+    assert geom.Equal(m, m2)
+
+if __name__ == "__main__":
+  try:
+    unittest.main()
+  except Exception, e:
+    print e
