25982f56086355e87d221cc09ea321940d5879e1
Tobias Schmidt <tobias.schmidt@unibas.ch>
2012-02-23 16:18:26 +0100

    fix operator overloading for ResNum (to make it compile with gcc 4.3)

--- modules/mol/base/src/residue_prop.hh
+++ modules/mol/base/src/residue_prop.hh
@@ -28,9 +28,10 @@ namespace ost { namespace mol {
 
 class DLLEXPORT ResNum: private
     boost::additive<ResNum, int,
+    boost::additive<ResNum,
     boost::totally_ordered<ResNum, 
     boost::totally_ordered<ResNum, int,
-    boost::unit_steppable<ResNum> > > >
+    boost::unit_steppable<ResNum> > > > >
 {
 public:
   ResNum(int n):
@@ -73,16 +74,6 @@ public:
   {
     num_-=r.num_;
     return num_;
-  }
-
-  ResNum operator+(const ResNum& r)
-  {
-    return ResNum(num_+r.num_);
-  }
-  
-  ResNum operator-(const ResNum& r)
-  {
-    return ResNum(num_-r.num_);
   }
   
   ResNum& operator++()
