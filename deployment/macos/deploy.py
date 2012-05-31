06b2ec00526786fa1d3a199609d9842d68984e4b
Marco Biasini <marco.biasini@unibas.ch>
2012-05-31 17:36:28 +0200

    fix deployment on MacOS X

--- deployment/macos/deploy.py
+++ deployment/macos/deploy.py
@@ -20,8 +20,7 @@ bundle.create_bundle('DNG', opts.bundle)
 if opts.bundle:
   shutil.copytree('../../examples', 'DNG.app/Contents/examples')
   os.system('rm `find DNG.app/Contents/examples/ -name "*.pyc"` 2> /dev/null')
-  os.system('rm -rf DNG.app/Contents/examples/demos/harmony')
-  os.system('rm -rf DNG.app/Contents/examples/demos/dokk')
+  os.system('rm -rf DNG.app/Contents/examples/code_fragments/')
   if opts.dmg:
     os.system('rm -rf openstructure.dmg')
     os.system('hdiutil create -srcFolder DNG.app openstructure.dmg')
