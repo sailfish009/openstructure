7e87fd6059021a76f9b5095aff1095b3484f1e67
Valerio Mariani <valerio.mariani@unibas.ch>
2012-05-31 03:24:56 +0200

    Added helper function to fill ClashingDistances

--- modules/mol/alg/pymod/__init__.py
+++ modules/mol/alg/pymod/__init__.py
@@ -3,3 +3,10 @@ from ost.mol.alg.superpose import *
 import ost.mol.alg.trajectory_analysis
 import ost.mol.alg.structure_analysis
 import ost.mol.alg.helix_kinks
+
+
+def FillClashingDistancesFromFile(filename,default_clashing_distance,default_tolerance):
+  fh=open(filename,'r')
+  lines=fh.readlines()
+  fh.close()
+  return FillClashingDistances(lines,default_clashing_distance,default_tolerance)
