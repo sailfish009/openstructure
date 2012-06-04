ba19c9a08a0a121b8ca78b11406180b4afa79c2c
Valerio Mariani <valerio.mariani@unibas.ch>
2012-06-04 02:45:51 +0200

    New helper functions and updated stereo chemical parameter file

--- modules/mol/alg/pymod/__init__.py
+++ modules/mol/alg/pymod/__init__.py
@@ -1,3 +1,4 @@
+import os.path
 from _ost_mol_alg import *
 from ost.mol.alg.superpose import *
 import ost.mol.alg.trajectory_analysis
@@ -10,3 +11,43 @@ def FillClashingDistancesFromFile(filename,default_clashing_distance,default_tol
   lines=fh.readlines()
   fh.close()
   return FillClashingDistances(lines,default_clashing_distance,default_tolerance)
+
+
+def FillBondStereoChemicalParamsFromFile(filename):
+  fh=open(filename,'r')
+  lines=fh.readlines()
+  fh.close()
+  return FillBondStereoChemicalParams("Bond",lines)
+
+
+def FillAngleStereoChemicalParamsFromFile(filename):
+  fh=open(filename,'r')
+  lines=fh.readlines()
+  fh.close()
+  return FillAngleStereoChemicalParams("Angle",lines)
+
+  
+def DefaultClashingDistances():
+  shared_path=ost.GetSharedDataPath()	
+  filename=os.path.join(shared_path,'stereo_chemical_props.txt')
+  fh=open(filename,'r')
+  lines=fh.readlines()
+  fh.close()
+  return FillClashingDistances(lines,1.5,0.0)
+
+def DefaultBondStereoChemicalParams():
+  shared_path=ost.GetSharedDataPath()	
+  filename=os.path.join(shared_path,'stereo_chemical_props.txt')
+  fh=open(filename,'r')
+  lines=fh.readlines()
+  fh.close()
+  return FillStereoChemicalParams("Bond",lines)
+
+def DefaultAngleStereoChemicalParams():
+  shared_path=ost.GetSharedDataPath()	
+  filename=os.path.join(shared_path,'stereo_chemical_props.txt')
+  fh=open(filename,'r')
+  lines=fh.readlines()
+  fh.close()
+  return FillStereoChemicalParams("Angle",lines)
+
