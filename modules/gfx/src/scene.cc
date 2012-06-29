sailfish009/openstructure

commit 81d57f7860289fc6b320a7311d0b74288b52f413

added projection matrix to scene interface; added inverted matrix to transform

modules/gfx/src/scene.cc
@@ -2016,6 +2016,12 @@ void Scene::stereo_projection(int view)
     // standard viewing frustum
     glFrustum(left,right,bot,top,zn,zf);
   }
+
+  // TODO: generate both directly from near/far/fov
+  float pm[16];
+  glGetFloatv(GL_PROJECTION_MATRIX,pm);
+  pmat_=geom::Transpose(geom::Mat4(pm));
+  ipmat_=geom::Invert(pmat_);
 }
 
 void Scene::render_stereo()

modules/gfx/pymod/export_scene.cc
@@ -126,6 +126,10 @@ void export_Scene()
     .add_property("bg",
                   &Scene::GetBackground, 
                   &Scene::SetBackground)
+    .def("GetProjection",&Scene::GetProjection)
+    .add_property("projection",&Scene::GetProjection)
+    .def("GetInvertedProjection",&Scene::GetInvertedProjection)
+    .add_property("inverted_projection",&Scene::GetInvertedProjection)
     .def("SetNear",&Scene::SetNear)
     .def("GetNear",&Scene::GetNear)
     .add_property("near", &Scene::GetNear, &Scene::SetNear)

modules/gfx/src/scene.hh
@@ -142,6 +142,9 @@ class DLLEXPORT_OST_GFX Scene {
   /// one of fallback, basic, default, hf, toon1, toon2
   void SetShadingMode(const std::string& smode);
 
+  geom::Mat4 GetProjection() const {return pmat_;}
+  geom::Mat4 GetInvertedProjection() const {return ipmat_;}
+
   /// \name clipping planes, fog and field-of-view
   //@{
   /// \brief get near clipping plane
@@ -514,6 +517,7 @@ private:
   float znear_,zfar_; // near and far clipping plane
   float fnear_,ffar_; // fog near and far offsets
 
+  geom::Mat4 pmat_,ipmat_; // projection and inverted projection matrix
   unsigned int vp_width_,vp_height_; // viewport
 
   SceneViewStack scene_view_stack_;

modules/mol/base/pymod/CMakeLists.txt
@@ -18,6 +18,7 @@ export_bounding_box.cc
 export_query_view_wrapper.cc
 export_torsion.cc
 export_visitor.cc
+export_transform.cc
 wrap_mol.cc
 export_entity_property_mapper.cc
 )

modules/mol/base/pymod/export_transform.cc
@@ -1,0 +1,63 @@
+//------------------------------------------------------------------------------
+// This file is part of the OpenStructure project <www.openstructure.org>
+//
+// Copyright (C) 2008-2011 by the OpenStructure authors
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 3.0 of the License, or (at your option)
+// any later version.
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//------------------------------------------------------------------------------
+#include <boost/python.hpp>
+
+#include <ost/mol/transform.hh>
+#if(OST_INFO_ENABLED)
+#include <ost/info/info.hh>
+#endif
+
+using namespace boost::python;
+using namespace ost;
+using namespace ost::mol;
+
+void export_Transform()
+{
+  class_<Transform>("Transform", init<>())
+    .def(init<const Transform&>()) // shouldn't this be there automatically ?
+    .def("GetMatrix",&Transform::GetMatrix)
+    .def("SetMatrix",&Transform::SetMatrix)
+    .add_property("matrix",&Transform::GetMatrix,&Transform::SetMatrix)
+    .def("GetTransposedMatrix",&Transform::GetTransposedMatrix)
+    .add_property("tmatrix",&Transform::GetTransposedMatrix)
+    .add_property("transposed_matrix",&Transform::GetTransposedMatrix)
+    .def("GetInvertedMatrix",&Transform::GetInvertedMatrix)
+    .add_property("inverted_matrix",&Transform::GetInvertedMatrix)
+    .def("SetTrans",&Transform::SetTrans)
+    .def("GetTrans",&Transform::GetTrans)
+    .add_property("trans",&Transform::GetTrans,&Transform::SetTrans)
+    .def("SetCenter",&Transform::SetCenter)
+    .def("GetCenter",&Transform::GetCenter)
+    .add_property("center",&Transform::GetCenter,&Transform::SetCenter)
+    .def("SetRot",&Transform::SetRot)
+    .def("GetRot",&Transform::GetRot)
+    .add_property("rot",&Transform::GetRot,&Transform::SetRot)
+    .def("ApplyXAxisRotation",&Transform::ApplyXAxisRotation)
+    .def("ApplyYAxisRotation",&Transform::ApplyYAxisRotation)
+    .def("ApplyZAxisRotation",&Transform::ApplyZAxisRotation)
+    .def("ApplyXAxisTranslation",&Transform::ApplyXAxisTranslation)
+    .def("ApplyYAxisTranslation",&Transform::ApplyYAxisTranslation)
+    .def("ApplyZAxisTranslation",&Transform::ApplyZAxisTranslation)
+    .def("ApplyAxisRotation",&Transform::ApplyAxisRotation)
+    ;
+#if(OST_INFO_ENABLED)
+  def("TransformToInfo", &TransformToInfo);
+  def("TransformFromInfo", &TransformFromInfo);
+#endif
+}

modules/mol/base/pymod/wrap_mol.cc
@@ -17,11 +17,7 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 //------------------------------------------------------------------------------
 #include <boost/python.hpp>
-#include <ost/mol/transform.hh>
 #include <ost/mol/editor_base.hh>
-#if(OST_INFO_ENABLED)
-#include <ost/info/info.hh>
-#endif
 
 using namespace boost::python;
 using namespace ost::mol;
@@ -46,6 +42,8 @@ void export_PropertyID();
 void export_BoundingBox();
 void export_QueryViewWrapper();
 void export_EntityPropertyMapper();
+void export_Transform();
+
 BOOST_PYTHON_MODULE(_ost_mol)
 {
   enum_<EditMode>("EditMode")
@@ -73,33 +71,5 @@ BOOST_PYTHON_MODULE(_ost_mol)
   export_BoundingBox();
   export_QueryViewWrapper();
   export_EntityPropertyMapper();
-  class_<Transform>("Transform", init<>())
-    .def(init<const Transform&>()) // shouldn't this be there automatically ?
-    .def("GetMatrix",&Transform::GetMatrix)
-    .def("SetMatrix",&Transform::SetMatrix)
-    .add_property("matrix",&Transform::GetMatrix,&Transform::SetMatrix)
-    .def("GetTransposedMatrix",&Transform::GetTransposedMatrix)
-    .add_property("tmatrix",&Transform::GetTransposedMatrix)
-    .def("SetTrans",&Transform::SetTrans)
-    .def("GetTrans",&Transform::GetTrans)
-    .add_property("trans",&Transform::GetTrans,&Transform::SetTrans)
-    .def("SetCenter",&Transform::SetCenter)
-    .def("GetCenter",&Transform::GetCenter)
-    .add_property("center",&Transform::GetCenter,&Transform::SetCenter)
-    .def("SetRot",&Transform::SetRot)
-    .def("GetRot",&Transform::GetRot)
-    .add_property("rot",&Transform::GetRot,&Transform::SetRot)
-    .def("ApplyXAxisRotation",&Transform::ApplyXAxisRotation)
-    .def("ApplyYAxisRotation",&Transform::ApplyYAxisRotation)
-    .def("ApplyZAxisRotation",&Transform::ApplyZAxisRotation)
-    .def("ApplyXAxisTranslation",&Transform::ApplyXAxisTranslation)
-    .def("ApplyYAxisTranslation",&Transform::ApplyYAxisTranslation)
-    .def("ApplyZAxisTranslation",&Transform::ApplyZAxisTranslation)
-    .def("ApplyAxisRotation",&Transform::ApplyAxisRotation)
-    ;
-#if(OST_INFO_ENABLED)
-  def("TransformToInfo", &TransformToInfo);
-  def("TransformFromInfo", &TransformFromInfo);
-#endif
-  
+  export_Transform();
 }

modules/mol/base/src/transform.cc
@@ -40,21 +40,11 @@ Transform::Transform():
   update_tm();
 }
     
-Mat4 Transform::GetMatrix() const
-{
-  return tm_;
-}
-
 void Transform::SetMatrix(const Mat4& m)
 {
   tm_=m;
   ttm_ = Transpose(tm_);
   update_components();
-}
-
-Mat4 Transform::GetTransposedMatrix() const
-{
-  return ttm_;
 }
 
 void Transform::SetTrans(const Vec3& t) 
@@ -212,6 +202,8 @@ void Transform::update_tm()
                0.0,0.0,1.0,-cen_[2],
                0.0,0.0,0.0,1.0);
   ttm_ = Transpose(tm_);
+  // TODO: calculate from rot, cen and trans
+  itm_ = Invert(tm_);
 }
 
 void Transform::update_components()

modules/mol/base/src/transform.hh
@@ -38,8 +38,9 @@ class DLLEXPORT_OST_MOL Transform {
 public:
   Transform();
 
-  geom::Mat4 GetMatrix() const;
-  geom::Mat4 GetTransposedMatrix() const;
+  geom::Mat4 GetMatrix() const {return tm_;}
+  geom::Mat4 GetTransposedMatrix() const {return ttm_;}
+  geom::Mat4 GetInvertedMatrix() const {return itm_;}
 
   void SetCenter(const geom::Vec3& c);
   geom::Vec3 GetCenter() const;
@@ -81,6 +82,7 @@ private:
   geom::Vec3 cen_;
   geom::Mat4 tm_;
   geom::Mat4 ttm_;
+  geom::Mat4 itm_;
 
   void update_tm();
   void update_components();

commit bd98238da5937b29cb1c42cdaf1057b40ae27f0e

better fix for problem introduced with 81d57f7860289fc6

modules/gfx/src/scene.cc
@@ -1968,6 +1968,37 @@ void Scene::render_glow()
 #endif  
 }
 
+namespace {
+  geom::Mat4 frustum(float left, float right, float bot, float top, float near, float far) {
+    float rl=1.0/(right-left);
+    float tb=1.0/(top-bot);
+    float fn=1.0/(far-near);
+    float tn=2.0*near;
+    return geom::Mat4(tn*rl, 0.0f, (right+left)*rl, 0.0f,
+                      0.0f, tn*tb, (top+bot)*tb, 0.0f,
+                      0.0f, 0.0f, -(far+near)*fn, -tn*far*fn,
+                      0.0f, 0.0f, -1.0f, 0.0f);
+  }
+
+  geom::Mat4 translate(float x, float y, float z) {
+    return geom::Mat4(1.0f, 0.0f, 0.0f, x,
+                      0.0f, 1.0f, 0.0f, y,
+                      0.0f, 0.0f, 1.0f, z,
+                      0.0f, 0.0f, 0.0f, 1.0f);
+  }
+
+  geom::Mat4 rotate(float a, float x, float y, float z) {
+    float c=cos(a);
+    float d=1.0-c;
+    float s=sin(a);
+    geom::Vec3 v=geom::Normalize(geom::Vec3(x,y,z));
+    return geom::Mat4(v[0]*v[0]*d+c, v[0]*v[1]*d-v[2]*s, v[0]*v[2]*d+v[1]*s, 0.0f, 
+                      v[1]*v[0]*d+v[2]*s, v[1]*v[1]*d+c, v[1]*v[2]*d-v[0]*s, 0.0f, 
+                      v[0]*v[2]*d-v[1]*s, v[1]*v[2]*d+v[0]*s, v[2]*v[2]*d+c, 0.0f,
+                      0.0f, 0.0f, 0.0f, 1.0f);
+  }
+}
+
 void Scene::stereo_projection(int view)
 {
   if(!gl_init_) return;
@@ -1981,6 +2012,8 @@ void Scene::stereo_projection(int view)
   Real left = -top*aspect_ratio_;
   Real right = -left;
 
+  pmat_=frustum(left,right,bot,top,zn,zf);
+
   if(view!=0) {
     
     Real ff=(view<0 ? -1.0 : 1.0);
@@ -1988,19 +2021,17 @@ void Scene::stereo_projection(int view)
 
     if(stereo_alg_==1) {
       // Toe-in method, easy but wrong
-      glFrustum(left,right,bot,top,zn,zf);
       Real dist = -transform_.GetTrans()[2]+stereo_distance_;
-      glTranslated(0.0,0.0,-dist);
-      glRotated(-180.0/M_PI*atan(0.1*ff/iod),0.0,1.0,0.0);
-      glTranslated(0.0,0.0,dist);
+      pmat_=pmat_*translate(0.0,0.0,-dist)*rotate(-atan(0.1*ff/iod),0.0,1.0,0.0)*translate(0.0,0.0,dist);
+
     } else {
       // correct off-axis frustims
 
       Real fo=-transform_.GetTrans()[2]+stereo_distance_;
 
+#if 0
       // correction of near clipping plane to avoid extreme drifting
       // of left and right view
-#if 0
       if(iod*zn/fo<2.0) {
         zn=2.0*fo/iod;
         zf=std::max(zn+Real(0.2),zf);
@@ -2010,21 +2041,16 @@ void Scene::stereo_projection(int view)
       Real sd = -ff*0.5*iod*zn/fo;
       left+=sd;
       right+=sd;
-
-      glFrustum(left,right,bot,top,zn,zf);
-      glTranslated(-ff*iod*0.5,0.0,0.0);
+      pmat_=frustum(left,right,bot,top,zn,zf)*translate(-ff*iod*0.5,0.0,0.0);
     }
 
   } else { // view==0
-    // standard viewing frustum
-    glFrustum(left,right,bot,top,zn,zf);
+    // standard viewing frustum, no need to modify above call
   }
 
-  // TODO: generate both directly from near/far/fov
+  glMultMatrix(geom::Transpose(pmat_).Data());
+
   try {
-    float pm[16];
-    glGetFloatv(GL_PROJECTION_MATRIX,pm);
-    pmat_=geom::Transpose(geom::Mat4(pm));
     ipmat_=geom::Invert(pmat_);
   } catch (geom::GeomException& e) {
     LOG_WARNING("caught GeomException in Scene::stereo_projection: " << e.what());