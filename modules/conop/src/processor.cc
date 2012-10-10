bbecb9927caea0751b563f95743f57d0389357d0
Gabriel Studer <gabriel.studer@stud.unibas.ch>
2012-10-10 17:43:58 +0200

    add limits

--- modules/conop/src/processor.cc
+++ modules/conop/src/processor.cc
@@ -16,6 +16,7 @@
 // along with this library; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 //------------------------------------------------------------------------------
+#include <limits>
 #include <ost/log.hh>
 #include <ost/mol/xcs_editor.hh>
 #include <ost/mol/bond_handle.hh>
