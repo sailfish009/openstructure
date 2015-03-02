e77d08a63fbda16f8d00d6c613a6a7f9f27243a6
Gabriel Studer <gabriel.studer@unibas.ch>
2015-03-02 18:10:57 +0100

    updated the dates in the copyright line of the headers

--- modules/base/pymod/__init__.py
+++ modules/base/pymod/__init__.py
@@ -1,7 +1,7 @@
 #------------------------------------------------------------------------------
 # This file is part of the OpenStructure project <www.openstructure.org>
 #
-# Copyright (C) 2008-2011 by the OpenStructure authors
+# Copyright (C) 2008-2015 by the OpenStructure authors
 #
 # This library is free software; you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
--- modules/gfx/src/gradient_manager.cc
+++ modules/gfx/src/gradient_manager.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2008-2011 by the OpenStructure authors
+// Copyright (C) 2008-2015 by the OpenStructure authors
 //
 // This library is free software; you can redistribute it and/or modify it under
 // the terms of the GNU Lesser General Public License as published by the Free
