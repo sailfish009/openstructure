1497ca83d6dc325c697b43761804201277200c2f
Gabriel Studer <gabriel.studer@unibas.ch>
2015-03-02 12:08:09 +0100

    it's not really necessary to print the dihedral angles, they are all the same anyway after editing

--- examples/code_fragments/misc/roll_helix.py
+++ examples/code_fragments/misc/roll_helix.py
@@ -14,16 +14,7 @@ for r in c.GetResidueList():
   
 edi.UpdateXCS()
 
-for r in c.GetResidueList():
-  phi=r.GetPhiTorsion()
-  psi=r.GetPsiTorsion()
-  if phi.IsValid(): 
-    print 'PHI %3.0f' % math.degrees(phi.GetAngle())
-  if psi.IsValid(): 
-    print 'PSI %3.0f'  % math.degrees(psi.GetAngle())
-
 g=gfx.Entity('helix', e.Select('aname=CA,N,O,C,CB'))
-#g=gfx.Entity('helix', e)
 scene.Add(g)
 scene.SetCenter(g.GetCenter())
 
