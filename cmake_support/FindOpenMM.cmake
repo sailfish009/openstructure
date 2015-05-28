f1793657d5441196d8ce5b00899ba30b27b5b096
Stefan Bienert <stefan.bienert@unibas.ch>
2015-05-28 15:43:18 +0200

    Introduced a plugin path variable for OpenMM (just the variable, its not used for now)

--- cmake_support/FindOpenMM.cmake
+++ cmake_support/FindOpenMM.cmake
@@ -4,9 +4,10 @@
 # - Find OpenMM
 # Find the native OpenMM includes and library
 # 
-#  OpenMM_INCLUDE_DIRS - OpenMM include dirs.
-#  OpenMM_LIBRARIES    - List of libraries when using OpenMM.
-#  OpenMM_FOUND        - True if OpenMM found.
+#  OPEN_MM_INCLUDE_DIR  - OpenMM include dirs.
+#  OPEN_MM_LIBRARIES    - List of libraries when using OpenMM.
+#  OPEN_MM_FOUND        - True if OpenMM found.
+#  OPEN_MM_PLUGIN_DIR   - Default for ressource plugins
 
 if (OPEN_MM_INCLUDE_DIR)
   set(OPEN_MM_FOUND TRUE)
@@ -28,4 +29,15 @@ set(OPEN_MM_INCLUDE_DIRS ${OPEN_MM_INCLUDE_DIR})
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (OpenMM DEFAULT_MSG OPEN_MM_LIBRARY OPEN_MM_INCLUDE_DIR)
 
-mark_as_advanced (OPEN_MM_LIBRARY OPEN_MM_INCLUDE_DIR)
+if (OPEN_MM_LIBRARY)
+  set(OPEN_MM_FOUND TRUE)
+  if (NOT OPEN_MM_PLUGIN_DIR)
+    set(OPEN_MM_PLUGIN_DIR "/usr/local/openmm/lib/plugins")
+  endif (NOT OPEN_MM_PLUGIN_DIR)
+  if (NOT EXISTS "${OPEN_MM_PLUGIN_DIR}")
+    message(FATAL_ERROR "OpenMM plugin directory does not exist: '${OPEN_MM_PLUGIN_DIR}'")
+  endif (NOT EXISTS "${OPEN_MM_PLUGIN_DIR}")
+endif (OPEN_MM_LIBRARY)
+
+mark_as_advanced (OPEN_MM_LIBRARY OPEN_MM_INCLUDE_DIR OPEN_MM_FOUND
+                  OPEN_MM_PLUGIN_DIR)
