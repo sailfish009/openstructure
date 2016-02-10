5e79ee9e687fbf5dc3bb5e39c2d5e018169b8d04
Gerardo Tauriello <gerardo.tauriello@unibas.ch>
2016-02-10 09:28:54 +0100

    Removed HMM class (replaced by ProfileHandle)

--- modules/seq/base/pymod/wrap_seq.cc
+++ modules/seq/base/pymod/wrap_seq.cc
@@ -20,12 +20,10 @@
 using namespace boost::python;
 
 void export_sequence();
-void export_hmm();
 void export_profile_handle();
 
 BOOST_PYTHON_MODULE(_ost_seq)
 {
   export_sequence();
-  export_hmm();
   export_profile_handle();
 }
