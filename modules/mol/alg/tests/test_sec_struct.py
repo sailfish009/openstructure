1b7f6ea2a695e1ae9d5af774b71c93a9b8180967
Gerardo Tauriello <gerardo.tauriello@unibas.ch>
2017-08-22 16:02:22 +0200

    SCHWED-2638: fixed/cleaned-up dssp-locate-check in unit tests

--- modules/mol/alg/tests/test_sec_struct.py
+++ modules/mol/alg/tests/test_sec_struct.py
@@ -9,20 +9,18 @@ class TestSecStruct(unittest.TestCase):
   def testSecStruct(self):
 
     # unit test only makes sense, when a dssp binary is around
-    dssp_path = None
     try:
-      dssp_path = settings.Locate("dssp")
+      # same check used in dssp binding
+      dssp_path = settings.Locate(['dsspcmbi', 'dssp', 'mkdssp'],
+                                  env_name='DSSP_EXECUTABLE')
     except:
-      try:
-        dssp_path = settings.Locate("mkdssp")
-      except:
-        print "Could not find dssp, could not compare sec struct assignment..."
-        return
+      print "Could not find DSSP, could not compare sec struct assignment..."
+      return
 
     dssp_ent = io.LoadPDB(os.path.join("testfiles", "1a0s.pdb"))
     ost_ent = io.LoadPDB(os.path.join("testfiles", "1a0s.pdb"))
 
-    dssp.AssignDSSP(dssp_ent, dssp_bin = dssp_path)
+    dssp.AssignDSSP(dssp_ent, dssp_bin=dssp_path)
     mol.alg.AssignSecStruct(ost_ent)
 
     for a, b in zip(dssp_ent.residues, ost_ent.residues):
