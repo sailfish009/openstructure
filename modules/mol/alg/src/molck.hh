c94750c5bcd06761869bb656fc94bb23adf9c046
Rafal Gumienny <guma44@gmail.com>
2018-02-23 16:59:13 +0100

    refactor: SCHWED-3126 One constructor for MolckSettings is enough

--- modules/mol/alg/src/molck.hh
+++ modules/mol/alg/src/molck.hh
@@ -25,32 +25,22 @@ struct MolckSettings{
   bool map_nonstd_res;
   bool assign_elem;
 
-  MolckSettings():   rm_unk_atoms(false), // Remove unknown and atoms not following the nomenclature
-                     rm_non_std(false), // Remove all residues not one of the 20 standard amino acids
-                     rm_hyd_atoms(true), // Remove hydrogen atoms
-                     rm_oxt_atoms(false), // Remove terminal oxygens
-                     rm_zero_occ_atoms(false), // Remove atoms with zero occupancy
-                     colored(false), // Whether the output should be colored
-                     map_nonstd_res(true), // Map non standard residues back to standard ones (e.g.: MSE->MET,SEP->SER,etc.)
-                     assign_elem(true){} // Clean up element column
-
-  MolckSettings(bool init_rm_unk_atoms,
-                bool init_rm_non_std,
-                bool init_rm_hyd_atoms,
-                bool init_rm_oxt_atoms,
-                bool init_rm_zero_occ_atoms,
-                bool init_colored,
-                bool init_map_nonstd_res,
-                bool init_assign_elem){
-     rm_unk_atoms = init_rm_unk_atoms;
-     rm_non_std = init_rm_non_std;
-     rm_hyd_atoms = init_rm_hyd_atoms;
-     rm_oxt_atoms = init_rm_oxt_atoms;
-     rm_zero_occ_atoms = init_rm_zero_occ_atoms;
-     colored = init_colored;
-     map_nonstd_res = init_map_nonstd_res;
-     assign_elem = init_assign_elem;
-  }
+  MolckSettings(bool init_rm_unk_atoms=false,
+                bool init_rm_non_std=false,
+                bool init_rm_hyd_atoms=true,
+                bool init_rm_oxt_atoms=false,
+                bool init_rm_zero_occ_atoms=false,
+                bool init_colored=false,
+                bool init_map_nonstd_res=true,
+                bool init_assign_elem=true):
+      rm_unk_atoms(init_rm_unk_atoms), // Remove unknown and atoms not following the nomenclature
+      rm_non_std(init_rm_non_std), // Remove all residues not one of the 20 standard amino acids
+      rm_hyd_atoms(init_rm_hyd_atoms), // Remove hydrogen atoms
+      rm_oxt_atoms(init_rm_oxt_atoms), // Remove terminal oxygens
+      rm_zero_occ_atoms(init_rm_zero_occ_atoms), // Remove atoms with zero occupancy
+      colored(init_colored), // Whether the output should be colored
+      map_nonstd_res(init_map_nonstd_res), // Map non standard residues back to standard ones (e.g.: MSE->MET,SEP->SER,etc.)
+      assign_elem(init_assign_elem){} // Clean up element column
 
   public:
     std::string ToString(){
