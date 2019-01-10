948dfeea4e7c8388ff228f9de94d32a6f52b0062
Gabriel Studer <gabriel.studer@unibas.ch>
2019-01-10 11:15:40 +0100

    cmake updates

--- cmake_support/FindSQLite3.cmake
+++ cmake_support/FindSQLite3.cmake
@@ -1,27 +1,20 @@
-# Try to find SQLite3
-#
 # Once done this will define
 # - Find SQLite3
 # Find the native SQLite3 includes and library
 # 
-#  SQLITE3_INCLUDE_DIRS - SQLite3 include dirs.
-#  SQLITE3_LIBRARIES    - List of libraries when using SQLite3.
-#  SQLITE3_FOUND        - True if SQLite3 found.
+#  SQLITE3_INCLUDE_DIRS  - SQLite3 include dir.
+#  SQLITE3_LIBRARIES     - List of libraries when using SQLite3.
+#  SQLITE3_FOUND         - True if SQLite3 found.
 
-find_path(SQLITE3_INCLUDE_DIR NAMES sqlite3.h)
+find_path(SQLITE3_INCLUDE_DIR sqlite3.h)
 find_library(SQLITE3_LIBRARY NAMES sqlite3)
 
 # handle the QUIETLY and REQUIRED arguments and set SQLITE3_FOUND to TRUE if 
 # all listed variables are TRUE
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SQLITE3 DEFAULT_MSG SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR)
-IF(SQLITE3_FOUND)
-	SET(SQLITE3_INCLUDE_DIRS ${SQLITE3_INCLUDE_DIR})
-	SET(SQLITE3_LIBRARIES ${SQLITE3_LIBRARY})
-ELSE(SQLITE3_FOUND)
-	SET(SQLITE3_INCLUDE_DIRS)
-	SET(SQLITE3_LIBRARIES)
-ENDIF(SQLITE3_FOUND)
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (SQLITE3 DEFAULT_MSG SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR)
 
 mark_as_advanced(SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR)
 
+SET(SQLITE3_INCLUDE_DIRS ${SQLITE3_INCLUDE_DIR})
+SET(SQLITE3_LIBRARIES ${SQLITE3_LIBRARY})
