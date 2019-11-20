ff2ca6411a1a40c041a3ef50ed6d1c3fed244baf
Gabriel Studer <gabriel.studer@unibas.ch>
2019-11-20 11:39:13 +0100

    increase test coverage of msms binding

--- modules/bindings/tests/test_msms.py
+++ modules/bindings/tests/test_msms.py
@@ -12,24 +12,32 @@ class TestMSMSBindings(unittest.TestCase):
     self.num_vert=55762
     self.ases=20051.928
     self.asas=21574.324
+    self.volume=60229.848
 
   def testCalculateSurface(self):
     surf=msms.CalculateSurface(self.protein,
                                msms_env='MSMSSERVER',
                                density=3,
-                               radius=1.4)[0]
-    assert self.num_vert==len(surf.GetVertexIDList()) \
-       and self.num_tri==len(surf.GetTriIDList()), \
-       "Number of surface vertices (%i) or triangles (%i) do not match precalculated values (%i/%i)"%(len(surf.GetVertexIDList()),len(surf.GetTriIDList()),self.num_vert,self.num_tri)
-  
-  def testCalculateSurfaceAres(self):
-     (msms_ases, msms_asas)=msms.CalculateSurfaceArea(self.protein, \
-                                                      msms_env='MSMSSERVER',
-                                                      density=3,
-                                                      radius=1.4)
-     assert self.ases==msms_ases[0] and self.asas==msms_asas[0], \
-     "SASA (%f) or SESA (%f) do not match precalculated values (%f/%f)"%(msms_asas[0],msms_ases[0],self.asas,self.ases)
+                               radius=1.4,
+                               attach_asa="asa",
+                               attach_esa="esa")[0]
+    self.assertEqual(self.num_vert, len(surf.GetVertexIDList()))
+    self.assertEqual(self.num_tri, len(surf.GetTriIDList()))
+ 
+  def testCalculateSurfaceArea(self):
+    (msms_ases, msms_asas)=msms.CalculateSurfaceArea(self.protein, \
+                                                     msms_env='MSMSSERVER',
+                                                     density=3,
+                                                     radius=1.4)
+    self.assertEqual(self.ases, msms_ases[0])
+    self.assertEqual(self.asas, msms_asas[0])
 
+  def testCalculateSurfaceVolume(self):
+    volume=msms.CalculateSurfaceVolume(self.protein, \
+                                       msms_env='MSMSSERVER',
+                                       density=3,
+                                       radius=1.4)
+    self.assertEqual(self.volume, volume)
 
 if __name__ == "__main__":
   # test if msms package is available on system, otherwise ignore tests
