a4246eceaedf740792843fa5dae9323611d85a84
Gabriel Studer <gabriel.studer@unibas.ch>
2020-02-12 21:09:20 +0100

    add pseudo counts according to Angermueller et al. 2012

--- modules/seq/alg/src/hmm_pseudo_counts.hh
+++ modules/seq/alg/src/hmm_pseudo_counts.hh
@@ -24,9 +24,146 @@
 
 namespace ost{ namespace seq{ namespace alg{
 
+
+class ContextProfileDB;
+typedef boost::shared_ptr<ContextProfileDB> ContextProfileDBPtr;
+
+
+class ContextProfile{
+
+public:
+
+ContextProfile(int length): length_(length), 
+                            data_(ContextProfile::DataSize(length), 0.0) { }
+
+ContextProfile(int length, Real* data): length_(length), 
+                            data_(ContextProfile::DataSize(length), 0.0) {
+  memcpy(&data_[0], data, data_.size() * sizeof(Real));
+}
+
+void SetWeight(int pos, char olc, Real weight) {
+  if(pos >= length_) {
+  	throw Error("Tried to access invalid pos in ContextProfile");
+  }
+  int olc_idx = ProfileColumn::GetIndex(olc);
+  if(olc_idx != -1) {
+    data_[pos*20 + olc_idx] = weight;
+  } else {
+  	throw Error("Invalid one letter code in ContextProfile");
+  }
+}
+
+void SetPseudoCount(char olc, Real count) {
+  int olc_idx = ProfileColumn::GetIndex(olc);
+  if(olc_idx != -1) {
+    data_[length_*20 + olc_idx] = count;
+  } else {
+  	throw Error("Invalid one letter code in ContextProfile");
+  }
+}
+
+void SetBias(Real bias) { data_.back() = bias; }
+
+const Real* GetWeights(int pos) const{ 
+  if(pos >= length_) {
+  	throw Error("Tried to access invalid pos in ContextProfile");
+  }
+  return &data_[pos*20];
+}
+
+Real GetWeight(int pos, char olc) {
+  if(pos >= length_) {
+    throw Error("Tried to access invalid pos in ContextProfile");
+  }
+  int olc_idx = ProfileColumn::GetIndex(olc);
+  if(olc_idx != -1) {
+    return data_[pos*20 + olc_idx];
+  } else {
+    throw Error("Invalid one letter code in ContextProfile");
+  }  
+}
+
+const Real* GetPseudoCounts() const { return &data_[length_*20]; }
+
+Real GetPseudoCount(char olc) {
+  int olc_idx = ProfileColumn::GetIndex(olc);
+  if(olc_idx != -1) {
+    return data_[length_*20 + olc_idx];
+  } else {
+    throw Error("Invalid one letter code in ContextProfile");
+  }    
+}
+
+Real GetBias() const { return data_.back(); }
+
+const std::vector<Real>& GetData() const { return data_; }
+
+int GetLength() const { return length_; }
+
+static int DataSize(int length) { return (length+1)*20+1; }
+
+private:
+int length_;
+// data organisation:
+// context weights in chunks of 20 (length_ chunks)
+// followed by 20 elements representing the context pseudo counts
+// last element is the bias
+std::vector<Real> data_;
+};
+
+
+class ContextProfileDB {
+
+public:
+
+ContextProfileDB() { } 
+
+void Save(const String& filename) const;
+
+static ContextProfileDBPtr Load(const String& filename);
+
+static ContextProfileDBPtr FromCRF(const String& filename);
+
+void AddProfile(const ContextProfile& profile){
+
+  // enforce same length for all profiles
+  if(!profiles_.empty()) {
+    if(profile.GetLength() != profiles_[0].GetLength()) {
+      throw Error("Require all profiles to be of same length");
+    }
+  }
+  profiles_.push_back(profile);
+}
+
+const ContextProfile& operator [](int idx) const {
+  return profiles_[idx];
+}
+
+const ContextProfile& at(int idx) const {
+  return profiles_.at(idx);
+}
+
+size_t size() const {
+  return profiles_.size();
+}
+
+size_t profile_length() const {
+  if(profiles_.empty()) {
+    throw Error("DB must contain profiles to get profile length");
+  }
+  return profiles_[0].GetLength();
+}
+
+private:
+std::vector<ContextProfile> profiles_;
+};
+
 void AddTransitionPseudoCounts(ost::seq::ProfileHandle& profile);
 
 void AddAAPseudoCounts(ost::seq::ProfileHandle& profile);
+
+void AddAAPseudoCounts(ost::seq::ProfileHandle& profile, 
+                       const ContextProfileDB& db);
 
 }}} // ns
 
