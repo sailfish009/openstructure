aea78e77172e4b3712c6da0456fabbe637b28369
Stefan Bienert <stefan.bienert@unibas.ch>
2020-02-13 15:34:02 +0100

    Bum copyright to 2020

--- doc/conf/conf.py
+++ doc/conf/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'OpenStructure'
-copyright = '2019, OpenStructure authors'
+copyright = '2020, OpenStructure authors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
